cc6c0144722613446a12915d1cd0d572a506c992
duvancorrea96 <duvancorrea96@gmail.com>
2020-05-02 00:22:56 -0500

    agregar mascota, listar ok

--- controllers/mascota.js
+++ controllers/mascota.js
@@ -2,6 +2,17 @@ require("../connection");
 const Mascota = require("../models/Mascota");
 
 const ctl = {
+  agregarMascota: async (id, tipo, nombre, propietario) => {
+    const mascota = new Mascota({
+      id,
+      tipo,
+      nombre,
+      propietario,
+    });
+
+    const mascotaSave = await mascota.save();
+    return mascotaSave;
+  },
   listarMascotas: async () => {
     const mascotas = await Mascota.find();
     return mascotas;
--- index.js
+++ index.js
@@ -2,7 +2,12 @@ require("./connection");
 const Mascota = require("./models/Mascota");
 const ctlMascotas = require("./controllers/mascota");
 
-ctlMascotas
-  .listarMascotas()
-  .then((mascotas) => console.log("Mascotas :D >>> ", mascotas))
-  .catch((err) => console.log("error >>> ", err));
+// ctlMascotas
+//   .listarMascotas()
+//   .then((mascotas) => console.log("Mascotas :D >>> ", mascotas))
+//   .catch((err) => console.log("error >>> ", err));
+
+// ctlMascotas
+//   .agregarMascota(2, "perro", "lasis", "Duvan")
+//   .then((mascota) => console.log("Mascota :D >>> ", mascota))
+//   .catch((err) => console.log("error >>> ", err));
