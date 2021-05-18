a0c1187baeb26a6cf64263093d91f69b09bbcd62
DuvanCorrea <duvancorrea96@gmail.com>
2021-05-18 10:42:14 -0500

    update

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ require("./connection");
 const Mascota = require("./models/Mascota");
 const ctlMascotas = require("./controllers/mascota");
 
-// ctlMascotas
-//   .listarMascotas()
-//   .then((mascotas) => console.log("Mascotas :D >>> ", mascotas))
-//   .catch((err) => console.log("error >>> ", err));
+ctlMascotas
+  .listarMascotas()
+  .then((mascotas) => console.log("Mascotas :D >>> ", mascotas))
+  .catch((err) => console.log("error >>> ", err));
 
 // ctlMascotas
 //   .agregarMascota(2, "perro", "lasis", "Duvan")
 //   .then((mascota) => console.log("Mascota :D >>> ", mascota))
 //   .catch((err) => console.log("error >>> ", err));
 
-ctlMascotas
-  .encontrarUnoPorId(2)
-  .then((mascota) => console.log("Mascota por id :D >>> ", mascota))
-  .catch((err) => console.log("error >>> ", err));
+// ctlMascotas
+//   .encontrarUnoPorId(2)
+//   .then((mascota) => console.log("Mascota por id :D >>> ", mascota))
+//   .catch((err) => console.log("error >>> ", err));
